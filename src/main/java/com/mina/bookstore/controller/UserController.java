d3d6b6c5aeea78e6554d0b22c0ffee04724a5df8
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-02 15:41:24 +0200

    initial commit setup customer endpoint and DB integration

---
+++ src/main/java/com/mina/bookstore/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.mina.bookstore.controller;
+
+import com.mina.bookstore.model.User;
+import com.mina.bookstore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(path = "users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/get")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+}
