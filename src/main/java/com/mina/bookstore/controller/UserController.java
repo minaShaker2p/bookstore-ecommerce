44628f1d96e6c2bae6925c40f6b800feb442e803
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-02 16:16:12 +0200

    set add user functionality in User controller

--- src/main/java/com/mina/bookstore/controller/UserController.java
+++ src/main/java/com/mina/bookstore/controller/UserController.java
@@ -2,9 +2,14 @@ package com.mina.bookstore.controller;
 
 import com.mina.bookstore.model.User;
 import com.mina.bookstore.repository.UserRepository;
+import net.bytebuddy.implementation.bind.annotation.Pipe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +29,15 @@ public class UserController {
         return userRepository.findAll();
     }
 
+    @PostMapping("/add")
+    public void createUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public User deleteById(@PathVariable("id") Long id) {
+        User user = userRepository.getOne(id);
+        userRepository.deleteById(id);
+        return user;
+    }
 }
