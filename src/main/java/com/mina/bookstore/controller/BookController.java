ae7aeed944666c6efbf35b598cb9d605b9e5a5fa
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-03 17:25:04 +0200

    add new  Book endpoint in  controller

--- src/main/java/com/mina/bookstore/controller/BookController.java
+++ src/main/java/com/mina/bookstore/controller/BookController.java
@@ -5,15 +5,21 @@ import com.mina.bookstore.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path = "books")
 public class BookController {
+    private byte[] image;
 
     @Autowired
     private BookRepository bookRepository;
@@ -22,4 +28,17 @@ public class BookController {
     public List<Book> getBooks() {
         return bookRepository.findAll();
     }
+
+    @PostMapping("/upload")
+    public void uploadImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
+        this.image = file.getBytes();
+    }
+
+    @PostMapping("/add")
+    public void createBook(@RequestBody Book book) throws IOException {
+        book.setPicture(image);
+        bookRepository.save(book);
+        this.image = null;
+    }
+
 }
