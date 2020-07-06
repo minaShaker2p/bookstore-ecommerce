e48215722dd5a4e38fee9560a715a4460e231c18
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-06 11:01:08 +0200

    add update  Book endpoint in  controller

--- src/main/java/com/mina/bookstore/controller/BookController.java
+++ src/main/java/com/mina/bookstore/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,6 +40,10 @@ public class BookController {
         book.setPicture(image);
         bookRepository.save(book);
         this.image = null;
+    }
+    @PutMapping("/update")
+    public void updateBook(@RequestBody Book book) {
+        bookRepository.save(book);
     }
 
 }
